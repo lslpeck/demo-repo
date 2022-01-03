bdf3c1b8cb3c1060959f81962cb7dc50918bc91b
lslpeck <landonpeck@gmail.com>
2022-01-03 17:55:41 +0000

    17.55 commit.

--- data.manipulation.R
+++ data.manipulation.R
@@ -1,3 +1,12 @@
-#Data Manipulation
+#General Data Manipulation
+#install packages
+library(tidyverse)
+library(ggplot2)
+#read data (.csv)
 crop.data <-read.csv("crop.data.csv")
-mean(crop.data$density)
+#assign variables for data manipulation
+density.mean<-mean(crop.data$density)
+yield.mean<-mean(crop.data$yield)
+#plot
+plot(crop.data$yield)
+t.test(crop.data$density, crop.data$fertilizer)
